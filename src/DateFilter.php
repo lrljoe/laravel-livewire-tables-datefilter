889fb08eee9524f38cc241a15141d3705374d59d
Joe <104938042+lrljoe@users.noreply.github.com>
2024-07-13 00:36:55 +0000

    Test Implementation

---
+++ src/DateFilter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Rappasoft\LaravelLivewireTables\Plugins\Filters\DateFilter;
+
+use DateTime;
+use Rappasoft\LaravelLivewireTables\Views\Filter;
+use Rappasoft\LaravelLivewireTables\Views\Traits\Core\HasWireables;
+use Rappasoft\LaravelLivewireTables\Views\Traits\Filters\{HasConfig, IsStringFilter};
+
+class DateFilter extends Filter
+{
+    use HasConfig,
+        IsStringFilter;
+    use HasWireables;
+
+    public string $wireMethod = 'live';
+
+    protected string $view = 'livewire-tables-datefilter::date-filter';
+
+    protected string $configPath = 'livewire-tables.dateFilter.defaultConfig';
+
+    public function validate(string $value): string|bool
+    {
+        if (DateTime::createFromFormat('Y-m-d', $value) === false) {
+            return false;
+        }
+
+        return $value;
+    }
+
+    public function getFilterPillValue($value): ?string
+    {
+        if ($this->validate($value)) {
+            return DateTime::createFromFormat('Y-m-d', $value)->format($this->getConfig('pillFormat'));
+        }
+
+        return null;
+    }
+}
